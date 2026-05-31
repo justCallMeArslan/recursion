eb87332ac4bf350c98221cc23f5b8edbd878c21f
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-05-31 19:56:02 +0300

    Complete mergeSort exercise. Complete project

--- merge.js
+++ merge.js
@@ -4,3 +4,63 @@
 // Sort the right part
 // Merge them together 
 
+
+export function mergeSort(array, start, end) {
+    if (start === undefined) {
+        start = 0;
+    };
+    if (end === undefined) {
+        end = array.length - 1;
+    };
+    if (!Array.isArray(array)) {
+        return array;
+    };
+
+    if (start >= end) {
+        return array;
+    };
+    let mid = Math.floor((start + end) / 2);
+
+    mergeSort(array, start, mid); // splits left part recursevely
+    mergeSort(array, mid + 1, end); // splits right part recursevely
+
+
+    merge(array, start, mid, end);
+
+    return array;
+}
+
+function merge(array, start, mid, end) {
+    let temp = [];
+
+    let i = start;
+    let j = mid + 1;
+
+    while (i <= mid && j <= end) { // from start to end of their part (left and right parts)
+        // but important note that && is condition , so both part shoud be present at the time, 
+        // so array.length assumed to be even 
+        if (array[i] <= array[j]) {
+            temp.push(array[i]);
+            i++;
+        } else {
+            temp.push(array[j]);
+            j++;
+        }
+    }
+
+    while (i <= mid) { // if array is not even and left part was bigger
+        temp.push(array[i]);
+        i++;
+    }
+
+    while (j <= end) { // if array is not even and right part was bigger
+        temp.push(array[j]);
+        j++;
+    }
+
+    for (let k = 0; k < temp.length; k++) { // merged in temp items copied/passed 
+        // back to original array
+        array[start + k] = temp[k];
+    }
+}
+
--- merge.test.js
+++ merge.test.js
@@ -1,5 +1,22 @@
-// mergeSort([]) →[]
-// mergeSort([73]) →[73]
-// mergeSort([1, 2, 3, 4, 5]) →[1, 2, 3, 4, 5]
-// mergeSort([3, 2, 1, 13, 8, 5, 0, 1]) →[0, 1, 1, 2, 3, 5, 8, 13]
-// mergeSort([105, 79, 100, 110]) →[79, 100, 105, 110]
+import { mergeSort } from "./merge";
+
+test("empty array as input", () => {
+    expect(mergeSort([])).toEqual([]);
+})
+
+
+test("simple array", () => {
+    expect(mergeSort([73])).toEqual([73]);
+})
+
+test("correct array", () => {
+    expect(mergeSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+})
+
+test("large array", () => {
+    expect(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+})
+
+test("big numbers", () => {
+    expect(mergeSort([105, 79, 100, 110])).toEqual([79, 100, 105, 110]);
+})
